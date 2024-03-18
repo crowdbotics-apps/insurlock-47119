3bbb777a3ee4f34c7eeaa38bcea611555e49ee25
Crowdbotics <team@crowdbotics.com>
2024-03-18 14:52:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "insurlock-47119",
+  url: "https://insurlock-47119.botics.co",
+  api: "https://insurlock-47119.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
