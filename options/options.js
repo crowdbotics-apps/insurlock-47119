3a2fd2febf5b16c7bcc0bf701bf6d6d82db470b5
Luigi iuliani <luigi.iuliani@gmail.com>
2024-03-18 10:56:15 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "insurlock-47119",
   url: "https://insurlock-47119.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
